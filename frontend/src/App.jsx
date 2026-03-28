f8947cacb736170bafa680be7528121323bb39f2
Charly Islami <charlyislami19@gmail.com>
2026-03-28 13:07:28 +0700

    feat: add navbar with hover effect and FinBear branding

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Loading from "./pages/Loading";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -9,8 +11,17 @@ const App = () => {
       {isLoading ? (
         <Loading onFinish={() => setIsLoading(false)} />
       ) : (
-        <div>Home Page</div>
+        <div>
+        <Navbar />
+        <Routes>
+          <Route />
+          <Route />
+          <Route />
+        </Routes>
+      
+        </div>
       )}
+      
     </div>
   )
 };
