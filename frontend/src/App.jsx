d528efd710e0c9851ef0be5881f2311a9f28f1f6
Charly Islami <charlyislami19@gmail.com>
2026-04-07 22:48:04 +0700

    feat: learn page with some card and add 3D model teenager bear

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import ScrollToTop from "./components/ScrollToTop";
 import Profile from "./pages/Profile";
+import Learn from "./pages/Learn";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -31,6 +32,7 @@ const App = () => {
         >
           <Route index element={<Home />}/>
           <Route path="profile" element={<Profile />}/>
+          <Route path="belajar" element={<Learn />}/>
         </Route>
       </Routes>
     </> 
---
+++ frontend/src/pages/MateriDetail.jsx
@@ -0,0 +1,7 @@
+function MateriDetail() {
+    return(
+        <div>Materi Detail</div>
+    )
+}
+
+export default MateriDetail
